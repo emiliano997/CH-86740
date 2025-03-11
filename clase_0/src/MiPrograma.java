8225a1a183c5a6b6173e7824866e1368d4a30249
emilianoperez <emi.perez997@gmail.com>
2025-03-11 09:38:12 -0300

    [ADD] Clase 0

---
+++ clase_0/src/MiPrograma.java
@@ -0,0 +1,10 @@
+public class MiPrograma {
+  public static void main(String[] args) {
+    // Ejercicio
+    // Escribe un programa que imprima tus datos personales.
+    // Nombre, Apellido, Edad, Provincia, Actividad Favorita
+    System.out.println("Nombre: Emiliano");
+    System.out.println("Apellido: Perez");
+    System.out.println("Edad: 25");
+  }
+}
